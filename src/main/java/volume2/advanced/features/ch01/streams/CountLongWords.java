975535f042f80a0b8d05e93021d76fde10500abf
Rancho <ranchocooper@gmail.com>
2019-09-02 01:07:13 +0800

    count long words

---
+++ src/main/java/volume2/advanced/features/ch01/streams/CountLongWords.java
@@ -0,0 +1,40 @@
+package volume2.advanced.features.ch01.streams;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author rancho
+ * @date 2019-09-02
+ */
+public class CountLongWords {
+
+    public static void main(String[] args) throws IOException {
+
+        String contents = new String(Files.readAllBytes(Paths.get("/etc/hosts")), StandardCharsets.UTF_8);
+        List<String> words = Arrays.asList(contents.split("\\PL"));
+
+        long count = 0;
+
+        for (String w : words) {
+            if (w.length() > 12) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+        count = words.stream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+
+        count = words.parallelStream().filter(w -> w.length() > 12).count();
+        System.out.println(count);
+
+    }
+
+}
